be21a9e721aab2a8a31b69cdc94b2a0732d885c4
Arthur <arthur.brussee@gmail.com>
2024-11-26 17:58:39 +0000

    Prune some deps

--- crates/brush-viewer/src/panels/tracing_debug.rs
+++ crates/brush-viewer/src/panels/tracing_debug.rs
@@ -12,9 +12,6 @@ impl ViewerPanel for TracingPanel {
 
     fn ui(&mut self, ui: &mut egui::Ui, _: &mut ViewerContext) {
         ui.checkbox(&mut self.constant_redraw, "Constant redraw");
-        let mut checked = sync_span::is_enabled();
-        ui.checkbox(&mut checked, "Sync scopes");
-        sync_span::set_enabled(checked);
 
         // Nb: this redraws the whole context so this will include the splat views.
         if self.constant_redraw {
